a706332daab02c33ecb8c892597d79ee2fa4e360
Charlie Y <omelettebaker@gmail.com>
2018-05-02 00:46:20 -0700

    -Renamed variables to use underscore instead of camelcase. -Removed autoLink attributes and replaced with onClick to use Intents for opening website, phone dialer, and map -Fixed dimension for text size to use "sp" instead of "dp"

--- app/src/main/java/com/charliej/yoon/castellopta/MainActivity.java
+++ app/src/main/java/com/charliej/yoon/castellopta/MainActivity.java
@@ -1,8 +1,11 @@
 package com.charliej.yoon.castellopta;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,45 +15,74 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    // opens dialer to call school number.
+    public void dialNumber(View v) {
+        String number = getString(R.string.tel_number);
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse(number));
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    // opens PTA's website when link is clicked.
+    public void webClick(View v) {
+        String url = getString(R.string.web_address);
+        openBrowser(url);
+    }
+
     // opens PTA's Instagram page when the Instagram icon is clicked.
     public void instagramClick(View v) {
-        String url = "https://www.instagram.com/helencarrcastellopta/";
+        String url = getString(R.string.instagram_address);
         openBrowser(url);
     }
 
     // opens PTA's Facebook page when the Facebook icon is clicked.
     public void facebookClick(View v) {
-        String url = "https://www.facebook.com/HelenCarrCastelloPTA/";
+        String url = getString(R.string.facebook_address);
         openBrowser(url);
     }
 
     // opens PTA's Amazon Smile website when the Amazon icon is clicked.
     public void amazonClick(View v) {
-        String url = "https://smile.amazon.com/ch/20-3285880";
+        String url = getString(R.string.amazon_address);
         openBrowser(url);
     }
 
-    // open's link to download Castello's app from the Google Play Store.
+    // opens link to download Castello's app from the Google Play Store.
     public void playStoreClick(View v) {
-        String url = "https://play.google.com/store/apps/details?id=com.app_hcces.layout";
+        String url = getString(R.string.play_store_address);
         openBrowser(url);
     }
 
+    // opens link to download Castello's app from App Store.
     public void appStoreClick(View v) {
-        String url = "https://itunes.apple.com/us/app/castello-elementary/id964816008?mt=8";
+        String url = getString(R.string.app_store_address);
         openBrowser(url);
     }
 
     // method that defines intent to open web address.
     private void openBrowser(String address) {
-        android.content.Intent intent = new android.content.Intent();
-        intent.setAction(android.content.Intent.ACTION_VIEW);
-        intent.addCategory(android.content.Intent.CATEGORY_BROWSABLE);
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(address));
 
-        //pass the url to intent data
-        intent.setData(android.net.Uri.parse(address));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 
-        startActivity(intent);
+    public void getMap(View v) {
+        String location = getString(R.string.address_url);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(location));
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
     }
 
 }
